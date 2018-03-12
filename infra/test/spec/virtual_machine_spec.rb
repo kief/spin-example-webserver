820e5e6cc3d9eac7f9db4b68b73000a03a38c389
kief <kmorris@thoughtworks.com>
2018-03-12 16:14:49 +0000

    Updated server name we're looking for with the awspec test

--- infra/test/spec/virtual_machine_spec.rb
+++ infra/test/spec/virtual_machine_spec.rb
@@ -1,7 +1,7 @@
 require 'spin_helper'
 
 context "Application Servers in #{environment_name}" do
-  describe named_ec2_instances_in_env(environment_name, 'application_server') do
+  describe named_ec2_instances_in_env(environment_name, "application_server-#{environment_name}") do
     it { should have(1).items }
     it { is_expected.to all(exist) }
     it { is_expected.to all(be_running) }
