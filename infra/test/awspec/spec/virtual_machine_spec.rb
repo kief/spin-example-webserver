8ae01393d48a8697656f15fb464e1a6617c29c34
Vlad Romanenko <vlad.romanenko@hotmail.com>
2018-03-13 11:22:22 +0000

    Use RSpec 3 compatible syntax.

--- infra/test/awspec/spec/virtual_machine_spec.rb
+++ infra/test/awspec/spec/virtual_machine_spec.rb
@@ -2,7 +2,7 @@ require 'spin_helper'
 
 context "Application Servers in #{environment_name}" do
   describe ec2_instances_named_in_env(environment_name, "application_server-#{environment_name}") do
-    it { should have(1).items }
+    its(:size) { is_expected.to eq 1 }
     it { is_expected.to all(exist) }
     it { is_expected.to all(be_running) }
   end
