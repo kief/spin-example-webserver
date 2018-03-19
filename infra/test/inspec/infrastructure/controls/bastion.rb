b94c1d5b26a29b1394c83b56f8076b777d601b65
kief <kmorris@thoughtworks.com>
2018-03-19 15:06:33 +0000

    Reorganized the inspec controls

--- infra/test/inspec/infrastructure/controls/bastion.rb
+++ infra/test/inspec/infrastructure/controls/bastion.rb
@@ -1,14 +1,12 @@
 # encoding: utf-8
 
-title 'webserver infrastructure'
+title 'ssh bastion infrastructure'
 
 deployment_id = attribute('deployment_id', default: 'unknown', description: 'Which deployment_id to inspect')
 component = attribute('component', description: 'Which component things should be tagged')
-role = attribute('role', description: 'Which role things should be tagged')
 
-describe aws_ec2_instance(name: "#{role}-#{deployment_id}") do
+describe aws_ec2_instance(name: "#{component}-#{deployment_id}") do
   it { should be_running }
   its('tags') { should include(key: 'DeploymentIdentifier', value: deployment_id) }
   its('tags') { should include(key: 'Component', value: component) }
-  its('tags') { should include(key: 'Role', value: role) }
 end
---
+++ infra/test/inspec/infrastructure/controls/servers.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+title 'servers'
+
+deployment_id = attribute('deployment_id', default: 'unknown', description: 'Which deployment_id to inspect')
+component = attribute('component', description: 'Which component things should be tagged')
+role = attribute('role', description: 'Which role things should be tagged')
+
+describe aws_ec2_instances(state_name: 'running') do
+  it { should have_instances }
+  its('count') { should eq 2 }
+  # TODO: can we say something like 'should_only include'?
+  its('image_id') { should include 'ami-63b0341a' }
+  its('name') { should include "#{role}-#{deployment_id}" }
+  its('name') { should include "#{component}-#{deployment_id}" }
+  # Redundant, but flags if I've botched the aws_ec2_instances code
+  its('states') { should include 'running' }
+end
